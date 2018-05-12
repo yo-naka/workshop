4b54be2cebe21bb045f58cc95b50d20e11e09177
Yuto Nagano <chamelion@hotmail.co.jp>
2018-05-12 17:36:20 +0900

    カードの生成方法を変更

--- cards-java/src/main/java/teamitp/Card.java
+++ cards-java/src/main/java/teamitp/Card.java
@@ -4,6 +4,11 @@ class Card {
     Suit suit;
     Rank rank;
 
+    Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
     @Override
     public String toString() {
         return String.format("{ Suit: %s; Rank: %s }", suit, rank);
