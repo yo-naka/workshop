5b4c052e90b6aa27c6489156fdb7ec3a285e29c0
Yuto Nagano <chamelion@hotmail.co.jp>
2018-05-12 15:24:09 +0900

    mvn archetype:generate で maven-archetype-quickstart を選択して生成

---
+++ cards-java/src/main/java/teamitp/App.java
@@ -0,0 +1,13 @@
+package teamitp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
